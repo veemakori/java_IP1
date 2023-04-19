5a4592742117aaeead958366b24d22d473f1177d
verah makori <verahmakori@gmail.com>
2023-04-19 07:51:50 +0300

    updated encode file

--- src/main/java/org/safmoringa/Encode.java
+++ src/main/java/org/safmoringa/Encode.java
@@ -5,17 +5,15 @@ public class Encode {
 
     public static String encrypt(String msg, int key){
         StringBuilder output = new StringBuilder();
-        for(char c: msg.toCharArray()){
-            int charPos = alphabets.indexOf(c);
-            if (charPos == -1){
-                output.append(c);
-                continue;
+        for (int i = 0; i< msg.length(); i++)
+        { char c = msg.charAt(i);
+            if( Character.isLetter(c))
+            {
+                c =(char) ((c - 'a'+ key + 26)% 26 + 'a');
             }
-            int tempNewPos = charPos + key;
-            int newPos = tempNewPos > 25 ?  tempNewPos % 26 : tempNewPos;
-            char replacement = alphabets.charAt(newPos);
-            output.append(replacement);
+            output.append(c);
         }
         return output.toString();
     }
+
 }
